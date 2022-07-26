3a066e01f3d4b6437401f847a5b054b80297346a
huni <piritaminkkinen@gmail.com>
2022-07-26 17:56:52 +0200

    mlee

---
+++ MTGTracker/Views/customButton.swift
@@ -0,0 +1,20 @@
+//
+//  customButton.swift
+//  MTGTracker
+//
+//  Created by Pirita on 26/07/2022.
+//
+
+import SwiftUI
+
+struct customButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct customButton_Previews: PreviewProvider {
+    static var previews: some View {
+        customButton()
+    }
+}
---
+++ MTGTracker/Views/hpMenu.swift
@@ -0,0 +1,20 @@
+//
+//  hpMenu.swift
+//  MTGTracker
+//
+//  Created by Pirita on 26/07/2022.
+//
+
+import SwiftUI
+
+struct hpMenu: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct hpMenu_Previews: PreviewProvider {
+    static var previews: some View {
+        hpMenu()
+    }
+}
