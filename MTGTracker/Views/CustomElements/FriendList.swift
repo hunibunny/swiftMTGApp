22969394e914a377083011fb6c721be7e89efa4b
huni <piritaminkkinen@gmail.com>
2022-08-02 16:31:23 +0200

    edited tracker setup but couldnt find a way to pass variables well yet

---
+++ MTGTracker/Views/CustomElements/FriendList.swift
@@ -0,0 +1,20 @@
+//
+//  FriendList.swift
+//  MTGTracker
+//
+//  Created by Pirita on 02/08/2022.
+//
+
+import SwiftUI
+
+struct FriendList: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FriendList_Previews: PreviewProvider {
+    static var previews: some View {
+        FriendList()
+    }
+}
