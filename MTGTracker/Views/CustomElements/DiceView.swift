834f6f4c352eed1cb2fc441a277598f1f8cfed57
huni <piritaminkkinen@gmail.com>
2022-08-08 17:32:51 +0200

    added kite and some dices

---
+++ MTGTracker/Views/CustomElements/DiceView.swift
@@ -0,0 +1,20 @@
+//
+//  DiceView.swift
+//  MTGTracker
+//
+//  Created by Pirita on 08/08/2022.
+//
+
+import SwiftUI
+
+struct DiceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct DiceView_Previews: PreviewProvider {
+    static var previews: some View {
+        DiceView()
+    }
+}
