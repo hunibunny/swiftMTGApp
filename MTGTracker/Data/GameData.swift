1068d7022c26add02edb0ad73e39af827189de3a
huni <piritaminkkinen@gmail.com>
2022-08-24 17:56:10 +0200

    made it prettier :)

---
+++ MTGTracker/Data/GameData.swift
@@ -0,0 +1,20 @@
+//
+//  GameData.swift
+//  MTGTracker
+//
+//  Created by Pirita on 24/08/2022.
+//
+
+import SwiftUI
+
+struct GameData: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct GameData_Previews: PreviewProvider {
+    static var previews: some View {
+        GameData()
+    }
+}
