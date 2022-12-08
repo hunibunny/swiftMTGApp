83995b4c229e2a749d4464a678f488c84af1d47e
hunibunny <piritaminkkinen@gmail.com>
2022-12-08 13:06:22 +0100

    saving after editing doesn't work????

--- Tracker/Views/Pages/CounterView.swift
+++ Tracker/Views/Pages/CounterView.swift
@@ -16,48 +16,48 @@ struct CounterView: View {
         ZStack{
             if modelData.currentGame!.ammountOfPlayers == 2{
                 VStack(spacing: 0){
-                    ScreenButton(rotation: 180, topColor: modelData.currentGame!.playerArray![0].topDataColor!.color, bottomColor: modelData.currentGame!.playerArray![0].bottomDataColor!.color, idealHeight: nil, hp: Int(modelData.currentGame!.hpArray![0].hp))
-                    ScreenButton(rotation: 0, topColor: modelData.currentGame!.playerArray![1].topDataColor!.color, bottomColor: modelData.currentGame!.playerArray![1].bottomDataColor!.color, idealHeight: nil, hp: Int(modelData.currentGame!.hpArray![1].hp))
+                    ScreenButton(rotation: 180, topColor: modelData.currentGame!.playerArray![0].topUIColor, bottomColor: modelData.currentGame!.playerArray![0].bottomUIColor, idealHeight: nil, hp: Int(modelData.currentGame!.hpArray![0].hp))
+                    ScreenButton(rotation: 0, topColor: modelData.currentGame!.playerArray![1].topUIColor, bottomColor: modelData.currentGame!.playerArray![1].bottomUIColor, idealHeight: nil, hp: Int(modelData.currentGame!.hpArray![1].hp))
                 }
             }
             else if modelData.currentGame!.ammountOfPlayers == 3{
                 VStack(spacing: 0){
                     HStack(spacing: 0){
                         GeometryReader{viewSize in
-                            ScreenButton(rotation: 90, topColor: modelData.currentGame!.playerArray![0].topDataColor!.color, bottomColor: modelData.currentGame!.playerArray![0].bottomDataColor!.color, idealHeight: nil, hp: Int(modelData.currentGame!.hpArray![0].hp))
+                            ScreenButton(rotation: 90, topColor: modelData.currentGame!.playerArray![0].topUIColor, bottomColor: modelData.currentGame!.playerArray![0].bottomUIColor, idealHeight: nil, hp: Int(modelData.currentGame!.hpArray![0].hp))
                                 .frame(width: viewSize.size.height ,height: viewSize.size.width/2)
                                 .position(x: viewSize.size.width/4, y: viewSize.size.height/2)
                             
-                            ScreenButton(rotation: -90, topColor: modelData.currentGame!.playerArray![1].topDataColor!.color, bottomColor: modelData.currentGame!.playerArray![1].bottomDataColor!.color, idealHeight: nil, hp: Int(modelData.currentGame!.hpArray![1].hp))
+                            ScreenButton(rotation: -90, topColor: modelData.currentGame!.playerArray![1].topUIColor, bottomColor: modelData.currentGame!.playerArray![1].bottomUIColor, idealHeight: nil, hp: Int(modelData.currentGame!.hpArray![1].hp))
                                 .frame(width: viewSize.size.height ,height: viewSize.size.width/2)
                                 .position(x: viewSize.size.width*3/4, y: viewSize.size.height/2)
                         }
                             
                     }
                     
-                    ScreenButton(rotation: 0, topColor: modelData.currentGame!.playerArray![2].topDataColor!.color, bottomColor: modelData.currentGame!.playerArray![2].bottomDataColor!.color, idealHeight: nil, hp: Int(modelData.currentGame!.hpArray![2].hp))
+                    ScreenButton(rotation: 0, topColor: modelData.currentGame!.playerArray![2].topUIColor, bottomColor: modelData.currentGame!.playerArray![2].bottomUIColor, idealHeight: nil, hp: Int(modelData.currentGame!.hpArray![2].hp))
                 }
             }
             else if modelData.currentGame!.ammountOfPlayers == 4{
                 VStack(spacing: 0){
                     HStack(spacing: 0){
                         GeometryReader{viewSize in
-                            ScreenButton(rotation: 90, topColor: modelData.currentGame!.playerArray![0].topDataColor!.color, bottomColor: modelData.currentGame!.playerArray![0].bottomDataColor!.color, idealHeight: nil, hp: Int(modelData.currentGame!.hpArray![0].hp))
+                            ScreenButton(rotation: 90, topColor: modelData.currentGame!.playerArray![0].topUIColor, bottomColor: modelData.currentGame!.playerArray![0].bottomUIColor, idealHeight: nil, hp: Int(modelData.currentGame!.hpArray![0].hp))
                                 .frame(width: viewSize.size.height ,height: viewSize.size.width/2)
                                 .position(x: viewSize.size.width/4, y: viewSize.size.height/2)
                         
-                            ScreenButton(rotation: -90, topColor: modelData.currentGame!.playerArray![1].topDataColor!.color, bottomColor: modelData.currentGame!.playerArray![1].bottomDataColor!.color, idealHeight: nil, hp: Int(modelData.currentGame!.hpArray![1].hp))
+                            ScreenButton(rotation: -90, topColor: modelData.currentGame!.playerArray![1].topUIColor, bottomColor: modelData.currentGame!.playerArray![1].bottomUIColor, idealHeight: nil, hp: Int(modelData.currentGame!.hpArray![1].hp))
                                 .frame(width: viewSize.size.height ,height: viewSize.size.width/2)
                                 .position(x: viewSize.size.width*3/4, y: viewSize.size.height/2)
                         }
                     }
                     HStack(spacing: 0){
                         GeometryReader{viewSize in
-                            ScreenButton(rotation: 90, topColor: modelData.currentGame!.playerArray![2].topDataColor!.color, bottomColor: modelData.currentGame!.playerArray![2].bottomDataColor!.color, idealHeight: nil, hp: Int(modelData.currentGame!.hpArray![2].hp))
+                            ScreenButton(rotation: 90, topColor: modelData.currentGame!.playerArray![2].topUIColor, bottomColor: modelData.currentGame!.playerArray![2].bottomUIColor, idealHeight: nil, hp: Int(modelData.currentGame!.hpArray![2].hp))
                                 .frame(width: viewSize.size.height ,height: viewSize.size.width/2)
                                 .position(x: viewSize.size.width/4, y: viewSize.size.height/2)
                         
-                            ScreenButton(rotation: -90, topColor: modelData.currentGame!.playerArray![3].topDataColor!.color, bottomColor: modelData.currentGame!.playerArray![3].bottomDataColor!.color, idealHeight: nil, hp: Int(modelData.currentGame!.hpArray![3].hp))
+                            ScreenButton(rotation: -90, topColor: modelData.currentGame!.playerArray![3].topUIColor, bottomColor: modelData.currentGame!.playerArray![3].bottomUIColor, idealHeight: nil, hp: Int(modelData.currentGame!.hpArray![3].hp))
                                 .frame(width: viewSize.size.height ,height: viewSize.size.width/2)
                                 .position(x: viewSize.size.width*3/4, y: viewSize.size.height/2)
                         }
